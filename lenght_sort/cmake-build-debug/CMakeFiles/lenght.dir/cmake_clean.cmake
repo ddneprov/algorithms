d20bdbb0346f251c916b4dcd5adef77f5974ef3f
Dneprov <44896272+ddneprov@users.noreply.github.com>
2019-02-13 23:57:08 +0300

    Add files via upload

---
+++ lenght_sort/cmake-build-debug/CMakeFiles/lenght.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lenght.dir/main.cpp.o"
+  "lenght.pdb"
+  "lenght"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lenght.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lenght_sort/cmake-build-debug/CMakeFiles/lenght.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lenght.
+# This may be replaced when dependencies are built.
