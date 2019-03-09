ef9f8d27109688a8fb84096ccc829c127d56aa38
Dneprov <44896272+ddneprov@users.noreply.github.com>
2019-03-09 20:06:46 +0300

    Add files via upload

---
+++ kruskal/main.cpp
@@ -0,0 +1,55 @@
+﻿#include "ReadWriter.h"
+//string, fstream, iostream, vector, algorithm, Edge.h - included in ReadWriter.h
+
+//Можно создавать любые классы и методы для решения задачи
+
+using namespace std;
+
+
+void solve(int N, int M, vector<Edge>& edges, vector<Edge>& result)
+{
+
+    vector <bool> used;
+    used.assign(N, false);
+
+
+    for (int i = 0; i < edges.size(); ++i) {
+        cout << edges[i].print() << endl;
+    }
+    sort(edges.begin(), edges.end(),
+            [] ( Edge left, Edge right ) { return left.W < right.W ;});
+
+    cout << "" << endl;
+    for (int j = 0; j < edges.size(); ++j) {
+        cout << edges[j].print() << endl;
+    }
+
+}
+
+int main()
+{
+    ReadWriter rw;
+    //Входные параметры
+    //N - количество вершин, M - количество ребер в графе
+    int N, M;
+    rw.read2Ints(N, M);
+
+    //Вектор ребер, каждое ребро представлено 3-мя числами (А, В, W), где A и B - номера вершин, которые оно соединяет, и W - вес ребра
+    //Основной структурой выбран вектор, так как из него проще добавлять и удалять элементы (а такие операции могут понадобиться).
+    vector<Edge> edges;
+    rw.readEgdes(M, edges);
+
+    //Основной структурой для ответа выбран вектор, так как в него проще добавлять новые элементы.
+    //(а предложенные алгоритмы работают итеративно, увеличивая количество ребер входящих в решение на каждом шаге)
+    vector<Edge> result;
+
+    //Алгоритм решения задачи
+    //В решение должны входить ребра из первоначального набора!
+    solve(N, M, edges, result);
+
+    //Выводим результаты
+    rw.writeInt(result.size());
+    rw.writeEdges(result);
+
+    return 0;
+}
