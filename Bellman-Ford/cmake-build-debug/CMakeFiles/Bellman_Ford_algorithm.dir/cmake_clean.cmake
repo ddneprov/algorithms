5d91098cc46c6dffd2968e87151cf5fbe6bc8b97
Dneprov <44896272+ddneprov@users.noreply.github.com>
2019-04-04 22:48:22 +0300

    Add files via upload

---
+++ Bellman-Ford/cmake-build-debug/CMakeFiles/Bellman_Ford_algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Bellman_Ford_algorithm.dir/main.cpp.o"
+  "Bellman_Ford_algorithm.pdb"
+  "Bellman_Ford_algorithm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bellman_Ford_algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
