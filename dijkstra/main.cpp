e8f813b28cc2b64fdb02c5caf24316af3f945093
Dneprov <44896272+ddneprov@users.noreply.github.com>
2019-04-08 00:04:05 +0300

    Add files via upload

--- dijkstra/main.cpp
+++ dijkstra/main.cpp
@@ -1,21 +1,90 @@
 ﻿#include "ReadWriter.h"
-//string, fstream, iostream, vector, Edge.h - included in ReadWriter.h
+#include <queue>
+#include <algorithm>
 
-//Можно создавать любые классы и методы для решения задачи
+
 
 using namespace std;
 
-//Основной метод решения задачи, параметры:
-//N - количество вершин, M - количество ребер в графе
-//edges - вектор ориентированных ребер, каждое ребро представлено 3-мя числами (А, В, W),
-// где A и B - номера вершин, которые оно соединяет (Путь строго из А в В), и W - вес ребра
-//передается по ссылке (&), чтобы не копировать, изменять вектор и его значения можно.
-//Результат также в виде вектора кратчайших расстояний из 0-й вершины во все остальные начиная с 1-й, то есть N-1 значение должно быть
+void relax(Edge edge,vector<int>& result)
+{
+    if (result[edge.B] > result[edge.A] + edge.W)
+        result[edge.B] = result[edge.A] + edge.W;
+}
+
+
+vector<int> initialize(int N, vector<int>& result)
+{
+    for (int i = 0; i < N; ++i)
+        result.push_back(30001);
+    return result;
+}
+
 void solve(int N, int M, vector<Edge>& edges, vector<int>& result)
 {
-    //Советую разделить решение на логические блоки
+    initialize(N, result);
+    vector<bool> was_used(N); // помогает определить использовали ли мы вершину?
+    was_used[0] = true;
+    result[0] = 0;
 
+    vector<int> Q;
+    Q.push_back(0);
+
+    while (!Q.empty())
+    {
+        bool flag = false;// показывает нужно ли идти дальше по edges
+        ///vector <int> selected;
+
+        for (int i = 0; i < M; ++i) {
+            if (edges[i].A == Q[0]) {            /// отбираем нужные ребра
+                relax(edges[i], result);
+                if (std::find(Q.begin(), Q.end(), edges[i].B) == Q.end() && !was_used[edges[i].B])// сейчас не в Q и не испоьзовали?
+                {
+                    flag = true; // мы считываем уже нужные ребра => когда выйдем - break, тк в нашем случае ребра идут в упорядоченном порядке
+                    was_used[edges[i].B] = true;
+                    Q.push_back(edges[i].B);
+                    ///selected.push_back(edges[i].B); /// добавляем новые вершины
+                }
+            }
+            else if (flag)
+                break;
+        }
+
+        Q.erase(Q.begin()); // удаляем отработанный эл-те
+
+
+        /*/// вставляем в нужное место
+        for (int j = 0; j < selected.size(); ++j)
+        {
+            if (Q.empty())
+                Q.push_back(selected[j]);
+            else {
+                for (int k = 0; k < Q.size(); ++k) {
+                    if (result[selected[j]] < result[Q[k]])
+                    {
+                        if (k==0)
+                            Q.insert(Q.begin(), selected[j]);
+                        else
+                            Q.insert(Q.begin() + k - 1 , selected[j]);
+                        break;
+                    }
+                    else if(k == Q.size())
+                    {
+                        Q.push_back(selected[j]);
+                    }
+                }
+            }
+        }
+
+        selected.clear();
+*/
+        // эта штуковина медленно работает, тк как сортирует все
+        std::sort(Q.begin(), Q.end(), [result](int left, int right){return result[left] < result[right];});
+    }
+    result.erase(result.begin());
 }
+
+
 
 int main()
 {
@@ -42,3 +111,6 @@ int main()
 
     return 0;
 }
+
+
+
